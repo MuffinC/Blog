fbb0081e161f7d635f68f21e3db5060e9b3fdabc
Mervyn_CJR <56317821+MuffinC@users.noreply.github.com>
2024-04-29 17:37:51 +0800

    img up and edit blogs
    upload image to database and creating blogs

--- firebase-blog-app/src/pages/AddEditBlog.js
+++ firebase-blog-app/src/pages/AddEditBlog.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import ReactTagInput from '@pathofdev/react-tag-input';
 import "@pathofdev/react-tag-input/build/index.css";
+import { storage } from '../firebase';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
 const initialState={
   title:"",
@@ -21,8 +23,41 @@ const categoryOption =[
 const AddEditBlog = () => {
   const [form, setForm] = useState(initialState);
   const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState(null);
 
   const {title, tags, category, trending, description} = form
+
+  useEffect(()=>{
+    const uploadFile =() =>{
+      const storageRef =ref(storage, file.name);
+      const uploadTask =  uploadBytesResumable(storageRef,file);
+
+      uploadTask.on("state_changed", (snapshot)=>{
+          const progress =(snapshot.bytesTransferred / snapshot.totalBytes)*100;
+          setProgress(progress);
+          switch (snapshot.state){
+            case "paused":
+              console.log("Upload is paused");
+              break;
+              case "running":
+              console.log("Upload is running");
+              break;
+              default:
+                break;
+
+          }
+      },(error) =>{
+        console.log(error)
+      }, ()=>{
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) =>{
+          setForm((prev)=> ({ ...prev, imgUrl:downloadUrl}));
+        });
+      } 
+    );
+    };
+    file && uploadFile();
+  },[file])
+
 
   const handleChange =(e) =>{};
 
@@ -37,7 +72,8 @@ const AddEditBlog = () => {
           <div className='text-center heading py-2'>
             Create Blog
           </div>
-          <div className='rows h-100 justify-content-center allign-items-center'>
+          </div>
+          <div className='row h-100 justify-content-center align-items-center'>
             <div className='col-10 col-md-8 col-lg-6'>
               <form  className='row blog-form'>
                 <div className='col-12 py-3'>
@@ -82,15 +118,35 @@ const AddEditBlog = () => {
 
                   <option> Please select Category</option>
                   {categoryOption.map((option, index) => (
-                    <option calue={option || ""} key={index}>{option}
+                    <option value={option || ""} key={index}>
+                    {option}
                     </option>
                   ))}
                 </select>
                 </div>
+                <div className='col-12 py-3'>
+                  <textarea 
+                  className='form-control description-box'
+                  placeholder='Description'
+                  value ={description}
+                  name="description"
+                  onChange={handleChange}
+                  />
+                </div>
+                 <div className='mb-3'>
+                  <input 
+                  type="file" 
+                  className='form-control'
+                  onChange ={(e)=>setFile(e.target.files[0])}/>
+                  </div> 
+                  <div className='col-12 py-3 text-center'>
+                    <button className='btn btn-add' type='submit'>
+                      Submit
+                    </button>
+                  </div>
               </form>
             </div>
           </div>
-        </div>
       </div>
     </div>
   )
