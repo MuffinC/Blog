ec52fcd8239c0c294c2f53f6dc632a6444561d57
Mervyn_CJR <56317821+MuffinC@users.noreply.github.com>
2024-05-13 17:22:42 +0800

    getting data from firebase
    blog gettting data from firebase

--- firebase-blog-app/src/pages/Home.js
+++ firebase-blog-app/src/pages/Home.js
@@ -1,6 +1,30 @@
-import React from 'react'
-
+import React,{useState, useEffect} from 'react'
+import {collection, onSnapshot} from 'firebase/firestore'
+import {db} from "../firebase"
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(()=>{
+    const unsub = onSnapshot(
+      collection(db, "blogs"),
+      (snapshot) =>{
+        let list =[];
+        snapshot.docs.forEach((docs)=>{
+          list.push({id: docs.id, ...docs.data()})
+        });
+        setBlogs(list);
+      }, (error) =>{
+        console.log(error)
+      }
+    );
+    return () =>{
+      unsub();
+    }
+  },[]);
+
+console.log("blogs", blogs);
+//2:10:13
   return (
     <div className='container-fluid pb-4 pt-4 padding'>
       <div className='container padding'>
